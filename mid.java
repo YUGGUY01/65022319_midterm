22886371212b736d7bc56227a3f8917f9d2eff44
YUGGUY01 <119989539+YUGGUY01@users.noreply.github.com>
2023-08-17 11:53:50 +0700

    Create mid.java

---
+++ mid.java
@@ -0,0 +1,82 @@
+class mid{
+    
+class Product{
+    private String Name;
+    private double price;
+
+    public String getName(){
+        System.out.println("chair");
+    
+    }
+    public setName(String name){
+        System.out.println();
+        
+    }
+    public double getPrice(){
+        System.out.println(500.0);
+        
+    }
+    public setPrice(double price){
+        System.out.println();
+    }
+
+}
+
+class Book{
+    private String auther;
+    private int pages;
+
+    public String getAuthor(){
+        System.out.println("OOP Programming");
+        
+    }
+    public setAuthor(String auther){
+    System.out.println("Author : John Doe");
+    
+    }
+    public int getpages(){
+        System.out.println("Product Price : " + 250.0);
+        
+    }
+    public setPages(int pages){
+        System.out.println("Number of Pages :" + 300);
+       
+    }
+} 
+
+class Electronics{
+    private String brand;
+    private String model;
+
+    public String getBrand(){
+        Systrm.out.println("Product Name : Television");
+       
+    }
+    public setBrand(String brand){
+        System.out.println("Brand : Sumsung");
+        
+    }
+    public String getModel(){
+        System.out.println("Model : Neo QLED");
+        
+    }
+    public setModel(String model){
+        System.out.println("Product Price : 20000.0");
+        
+    }
+    }
+
+public class test{
+    public static void main(String[] args) {
+        System.out.println("Product Name : " + name.setName());
+        System.out.println("Product Price : " + .getprice());
+        System.out.println("Product Name :  : " + auther.getName());
+        System.out.println("ทำไม่ทันค่าาา : " + .getprice());
+    }
+}
+}
+
+//ทำไม่ทันค่ะจารย์
+
+
+
